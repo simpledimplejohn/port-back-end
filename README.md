788a743994771e1beef476159fa9549df91f7e4e
John Blalock <simpledimplejohn@gmail.com>
2024-01-31 16:56:51 -0800

    update

--- README.md
+++ README.md
@@ -13,3 +13,20 @@ This project is a lightwieght full stack application that allows testing of port
 ## Setting Up Node
 `npm init -y`
 `npm install express`
+`npm install moment`
+To Run:
+`node server.js`
+To Stop:
+(Ctrl + C)
+
+## Commands to access the endpoint:
+`curl -X POST -H "Content-Type: application/json" -d '{"word":"potato"}' http://localhost:3000/log`
+
+- Using Postman (GUI Tool):
+    Open Postman.
+    Set the request type to POST.
+    Enter the URL http://localhost:3000/log.
+    Select the "Body" tab.
+    Choose "raw" and set the content type to "JSON".
+    Enter {"word":"potato"} in the request body.
+    Click "Send" to make the request.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
 
 const app = express();
 const PORT = 3000;
@@ -9,8 +10,19 @@ app.use(express.json());
 
 app.post('/log', (req, res) => {
     const { word } = req.body;
+
+    // Get current timestamp in the specified format
+    const timestamp = moment().format('MM/DD/YYYY HH:mm:ss.SSS Z');
+
+    // Create log message
+    const logMessage = `${timestamp} INFO Word = ${word}\n`;
+
+    // Log the message to the console
+    console.log(logMessage);
+
+    // Log the message to a file
     const logFilePath = path.join(__dirname, 'log', 'app_logs.log');
-    fs.appendFile(logFilePath, word + '\n', (err) => {
+    fs.appendFile(logFilePath, logMessage, (err) => {
         if (err) {
             console.error('Error writing to log file:', err);
             res.status(500).send('Error writing to log file');
@@ -22,5 +34,5 @@ app.post('/log', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}')
-})
+    console.log(`Server running on port ${PORT}`);
+});
